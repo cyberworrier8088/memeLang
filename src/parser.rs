4142775083ef9277314dd61083f9da44e90621b4
cyberworrier8088 <cyberworrier39@gmail.com>
2026-06-02 20:56:29 +0530

    add parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,1 +1,22 @@
-//commming. i am fully downed
+use crate::ast::{BinaryOp, Expr, Program, Stmt, UnaryOp};
+use crate::token::{Token, TokenK};
+
+pub struct Parser {
+    tokens: Vec<Token>,
+    current: usize,
+}
+
+impl Parser {
+    pub fn new (tokens: Vec<Token>) -> Self {
+        Self {tokens, current: 0}
+    }
+
+    pub fn parse_program(&mut self) -> Self {
+        let mut statements = Vec::new();
+        while !self.is_at_end() {
+            statements.push(self.statement()?)
+        }
+        Ok(Program { statements })
+
+    }
+}
